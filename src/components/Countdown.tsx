8a581cc1fa556bde52ddc78899335d138a1cfa4e
dazcalifornia <divertzt@gmail.com>
2023-08-29 18:39:57 +0700

    -

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -17,32 +17,55 @@ const Countdown = ({ targetDate }: any) => {
   };
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [countdownDone, setCountdownDone] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
+      const newTimeLeft = calculateTimeLeft();
+      setTimeLeft(newTimeLeft);
+
+      if (
+        newTimeLeft.days <= 0 &&
+        newTimeLeft.hours <= 0 &&
+        newTimeLeft.minutes <= 0 &&
+        newTimeLeft.seconds <= 0
+      ) {
+        setCountdownDone(true);
+        clearInterval(interval);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    if (countdownDone) {
+      // Redirect to www.google.com
+      window.location.href = "https://www.google.com";
+    }
+  }, [countdownDone]);
+
   return (
     <div className="text-center">
       <h1 className="text-4xl font-semibold mb-4">Countdown Timer</h1>
-      <div className="text-2xl">
-        <div>
-          <span className="font-semibold">{timeLeft.days}</span> days
+      {countdownDone ? (
+        <p className="text-xl">Countdown is done! Redirecting...</p>
+      ) : (
+        <div className="text-2xl">
+          <div>
+            <span className="font-semibold">{timeLeft.days}</span> days
+          </div>
+          <div>
+            <span className="font-semibold">{timeLeft.hours}</span> hours
+          </div>
+          <div>
+            <span className="font-semibold">{timeLeft.minutes}</span> minutes
+          </div>
+          <div>
+            <span className="font-semibold">{timeLeft.seconds}</span> seconds
+          </div>
         </div>
-        <div>
-          <span className="font-semibold">{timeLeft.hours}</span> hours
-        </div>
-        <div>
-          <span className="font-semibold">{timeLeft.minutes}</span> minutes
-        </div>
-        <div>
-          <span className="font-semibold">{timeLeft.seconds}</span> seconds
-        </div>
-      </div>
+      )}
     </div>
   );
 };
